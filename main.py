4d9b6bdc253552cd6388c08cfbc33355fa8846fa
champeman22 <36521400+champeman22@users.noreply.github.com>
2018-05-04 13:51:38 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from kivy.clock import Clock
 import time
+import os
+import random
 try:
     import pygame.mixer as mixer
 except ImportError:
@@ -29,12 +31,6 @@ class Alarm(BoxLayout):
         mixer.music.play()
         #need to add code to account for no user action
 
-    def pause(self):
-        mixer.music.pause()
-
-    def resume(self):
-        mixer.music.unpause()
-
     def snooze(self):
         mixer.music.pause()
         #print('You\'ve hit snooze', self.snoozeCounter, 'times.')
@@ -48,13 +44,47 @@ class Alarm(BoxLayout):
 
     def set_time(self,dt):
         self.current_time = time.strftime("%m/%d/%Y %H:%M")
+
+    def get_song(self):
+        DOTW=str(time.strftime('%A'))
+        songpath=os.getcwd()+'\\'+DOTW
+        choices=os.listdir(songpath)
+        #add default song if len(choices)=0
+        if len(choices)!=0:
+            song=choices[random.randint(0,len(choices)-1)]
+            #cycle through rather than pick random?
+            self.load(songpath+'\\'+song)
+        else:
+            song='RememberMe.mp3'
+            self.load(song)        
         
-class sample_kvfileApp(App):
+class MainScreenKVApp(App):
     def build(self):
         self.title = 'Champeman22 App'
-        cm = Alarm()
-        cm.load('RememberMe.mp3')
-        Clock.schedule_interval(cm.set_time, 0.1)
-        return cm
+        home = Alarm()
+        Popup=TimePickerKVApp()
+        alarm_time=Popup.set_alarm_time()
+        #add one day of seconds if time already passed today
+        if alarm_time-time.time()<0:
+            alarm_time+=86400
+        #sleep until the right time
+        time.sleep(alarm_time-time.time())
+        #Figure out threading here for the sleep piece
+        
+        #get song to play for the day
+        home.get_song()
+        home.play()
+        Clock.schedule_interval(home.set_time, 0.1)
+        return home
 
-sample_kvfileApp().run()
+class TimePickerKVApp(App):
+    def set_alarm_time(self):
+        #incorporate time picker
+        #time_input = str(raw_input("Please enter the time in HH:MM:SS format: "))
+        #use below for testing purposes
+        time_input='10:23:10'
+        current_date=time.strftime('%Y/%m/%d')
+        selected_time = time.mktime(time.strptime('%s %s'%(current_date, time_input),"%Y/%m/%d  %H:%M:%S"))
+        return selected_time
+
+MainScreenKVApp().run()
